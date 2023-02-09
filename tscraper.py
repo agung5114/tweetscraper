f836efed8050f7abdb6eddfbb43d1279a533ff8b
agung5114 <43102810+agung5114@users.noreply.github.com>
2023-02-10 01:45:08 +0700

    Update tscraper.py

--- tscraper.py
+++ tscraper.py
@@ -77,20 +77,20 @@ if st.button("Run Scraping"):
       st.write('No Data Found')
       data = []
 
-if data ==[]:
-   pass
-else:
-   c1, c2 = st.columns((1, 1))
-   with c1:
-      df1 = data
-      fig1 = px.pie(df1, names='Sentiment', values='Retweeted', color='Sentiment', hole=.6)
-      st.plotly_chart(fig1)
+   if data ==[]:
+      pass
+   else:
+      c1, c2 = st.columns((1, 1))
+      with c1:
+         df1 = data
+         fig1 = px.pie(df1, names='Sentiment', values='Retweeted', color='Sentiment', hole=.6)
+         st.plotly_chart(fig1)
 
-   with c2:
-      data['Date'] = pd.to_datetime(data['Datetime']).dt.date
-      df2b = data.groupby(by=['Sentiment','Date'],as_index=False).agg({'Retweeted':'sum'})
-      fig2 = px.bar(df2b, x='Province', y='Total', color='Disaster')
-      fig2.update_xaxes(title_text="")
-      st.plotly_chart(fig2)
+      with c2:
+         data['Date'] = pd.to_datetime(data['Datetime']).dt.date
+         df2b = data.groupby(by=['Sentiment','Date'],as_index=False).agg({'Retweeted':'sum'})
+         fig2 = px.bar(df2b, x='Province', y='Total', color='Disaster')
+         fig2.update_xaxes(title_text="")
+         st.plotly_chart(fig2)
     
     
